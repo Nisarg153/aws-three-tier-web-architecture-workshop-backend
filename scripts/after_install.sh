6f8a48620c6f151583b044a15dc4162bb491b6f9
Nisarg153 <nisarg150301@gmail.com>
2025-04-30 23:32:36 -0400

    appspec

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "📦 Installing backend dependencies..."
 
+# Change ownership to ec2-user
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app-tier
+
+# Switch into the directory
 cd /home/ec2-user/app-tier
+
+# Install dependencies
 npm install
